4b00ea67a4a483df191ea4828ed93c9daf07205c
Alberto <albythegenius@gmail.com>
2020-06-12 14:34:16 +0200

    Agginto DbContext allo Startup

--- Startup.cs
+++ Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SAL.DataAccess;
 
 namespace WebApplication1
 {
@@ -26,6 +28,8 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            var ConnString = Configuration.GetConnectionString("DbString");
+            services.AddDbContext<SALContext>(x => x.UseSqlServer(ConnString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
